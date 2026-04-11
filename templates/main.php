1767dc7aa191de4467c5f05e20497f15538b34f5
Mark <87275094+Bacaloo@users.noreply.github.com>
2026-04-11 14:43:44 +0200

    Scaffold Nextcloud app structure for Team4All

---
+++ templates/main.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+script('team4all', 'main');
+style('team4all', 'main');
+?>
+<div id="team4all-root" class="team4all-root">
+    <h2>Team4All</h2>
+    <p>Das Grundgerüst der App ist bereit.</p>
+</div>
