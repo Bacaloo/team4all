b49603f6c9cdee2248d2dd37a370996e6bab17a1
Mark <87275094+Bacaloo@users.noreply.github.com>
2026-04-11 15:02:45 +0200

    Relax Nextcloud dependency to min version 30 without upper bound

---
+++ appinfo/app.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Team4All\AppInfo;
+
+use OCP\AppFramework\App;
+
+$app = new App('team4all');
+
---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function () {
+    const root = document.getElementById('team4all-root');
+    if (!root) {
+        return;
+    }
+
+    root.dataset.initialized = 'true';
+})();
