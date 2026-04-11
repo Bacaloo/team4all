e4e1a846964fe527cc0901260aa4a6e54309ee8c
Mark <87275094+Bacaloo@users.noreply.github.com>
2026-04-11 15:34:49 +0200

    Update menu icon to new Team4All logo

---
+++ templates/main.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+script('team4all', 'main');
+style('team4all', 'main');
+?>
+<div id="team4all-root" class="team4all-root">
+    <img class="team4all-logo" src="<?= image_path('team4all', 'app.svg') ?>" alt="Team4All Logo" />
+    <h2>Team4All</h2>
+    <p>Das Grundgerüst der App ist bereit.</p>
+</div>
