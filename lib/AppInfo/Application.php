9be185ebd294b0f57c9677853db165f1c295553c
bacaloo <koop@hvob.immo>
2026-04-11 20:44:38 +0200

    Aktivierungssperre verbessert

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -12,6 +12,7 @@ use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
 class Application extends App implements IBootstrap {
     public const APP_ID = 'team4all';
+    private const REQUIRED_APP_ID = 'contacts';
 
     public function __construct(array $urlParams = []) {
         parent::__construct(self::APP_ID, $urlParams);
@@ -23,8 +24,12 @@ class Application extends App implements IBootstrap {
     public function boot(IBootContext $context): void {
         /** @var IAppManager $appManager */
         $appManager = $context->getServerContainer()->get(IAppManager::class);
-        if (!$appManager->isEnabledForAnyone('contacts')) {
-            throw new \RuntimeException('The Contacts app must be enabled system-wide before Team4All can be activated.');
+        if (!$appManager->isEnabledForAnyone(self::REQUIRED_APP_ID)) {
+            if (method_exists($appManager, 'disableApp')) {
+                $appManager->disableApp(self::APP_ID);
+            }
+
+            throw new \RuntimeException('The Contacts app must be enabled system-wide before Team4All can be used.');
         }
     }
 }
